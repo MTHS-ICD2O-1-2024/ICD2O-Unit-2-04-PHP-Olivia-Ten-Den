bbfee1d0943e620ff0abf70bae9c1ec2d61d7316
default <olivia.ten-den@stu.ocsb.ca>
2025-03-21 17:01:34 +0000

    added ending math for triangle

--- answer.php
+++ answer.php
@@ -4,7 +4,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Address program, PHP" />
+  <meta name="description" content="The Area of Triangle program, PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Olivia TD" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,7 +30,7 @@
     sizes="16x16"
     href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The Address program, PHP</title>
+  <title>The Area of Triangle program, PHP</title>
 </head>
 
 <body>
@@ -38,20 +38,26 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Address program, PHP!</span>
+        <span class="mdl-layout-title">The Area of Triangle program, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/notebook-animation.svg" alt="laptop image" width="250" />
+        <img src="./images/area_of_triangle.png" alt="White sign that shows the formula to calculate the area of a triangle, image" width="250" />
       </div>
       <div class="page-content-php">
         <div id="user-info">
           <?php
-          $number = $_GET["number"];
-          $name = $_GET["name"];
+          $baseOfTriangle = $_GET["base-of-triangle"];
+          $heightOfTriangle = $_GET["height-of-triangle"];
 
-          echo "<p>The address is: " . $number . " " . $name . ".</p>";
+          // process
+          $area = ($baseOfTriangle * $heightOfTriangle) / 2;
+
+          // output
+          echo "If the triangle has a base of = " . $baseOfTriangle . " cm and the height of = " . $heightOfTriangle . " cm:";
+          echo "<br />";
+          echo "Than the area of the triangle is " . $area . " cm².";
           ?>
         </div>
       </div>
